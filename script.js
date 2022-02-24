65668a31f9d9340417ae56ab70eea5a6dce17f21
Hazim <hazhaznur@gmail.com>
2022-02-23 22:17:29 -0500

    Logic implemented and UI updated

--- script.js
+++ script.js
@@ -3,22 +3,16 @@ const currentOp = document.querySelector(".current-op");
 const lastOp = document.querySelector(".last-op");
 const del = document.querySelector("#del");
 const clear = document.querySelector("#clear");
-const one = document.querySelector("#1");
-const two = document.querySelector("#2");
-const three = document.querySelector("#3");
-const four = document.querySelector("#4");
-const five = document.querySelector("#5");
-const six = document.querySelector("#6");
-const seven = document.querySelector("#7");
-const eight = document.querySelector("#8");
-const nine = document.querySelector("#9");
-const zero = document.querySelector("#0");
-const decimal = document.querySelector("#decimal");
-const equal = document.querySelector("#equal");
-const plus = document.querySelector("#plus");
-const subtr = document.querySelector("#subtr");
-const times = document.querySelector("#multiply");
-const divise = document.querySelector("#divise");
+const numBtn = document.querySelectorAll("[data-number]");
+const operationBtn = document.querySelectorAll("[data-operation]");
+const equal = document.querySelector("[data-equals]");
+let operator;
+let num1Arr = [];
+let num2Arr = [];
+let num1;
+let num2;
+let num1Split = [];
+let answer;
 
 const add = function (num1 = 0, num2) {
   return num1 + num2;
@@ -41,14 +35,108 @@ const division = function (num1 = 0, num2) {
 };
 
 const operate = function (operator, num1, num2) {
-  let operator;
   if (operator === "+") {
     return add(num1, num2);
   } else if (operator === "-") {
     return subtract(num1, num2);
-  } else if (operator === "*") {
+  } else if (operator === "x") {
     return multiply(num1, num2);
-  } else if (operator === "/") {
+  } else if (operator === "÷") {
     return division(num1, num2);
   }
 };
+
+console.log(operate("+", 5, 5));
+
+numBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.textContent === "." && currentOp.textContent.includes(".")) {
+      return;
+    } else if (currentOp.innerText === "0") {
+      currentOp.textContent = "";
+      currentOp.append(btn.innerText);
+      num1Arr.push(btn.innerText);
+      num1 = Number(num1Arr.join(""));
+    } else if (lastOp.innerText === "") {
+      currentOp.append(btn.innerText);
+      num1Arr.push(btn.innerText);
+      num1 = Number(num1Arr.join(""));
+    } else if (lastOp.innerText !== "") {
+      num2Arr.push(btn.innerText);
+      currentOp.append(btn.innerText);
+      num2 = Number(num2Arr.join(""));
+      num1Split = lastOp.textContent.split(" ");
+      answer = operate(num1Split[1], num1, num2);
+    }
+  });
+});
+
+console.log(num1Arr);
+
+operationBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (lastOp.innerText === "" && currentOp.innerText === "0") {
+      lastOp.textContent = `0 ${btn.textContent}`;
+      currentOp.textContent = "";
+      num1 = 0;
+      clear.classList.remove("now-clear");
+    } else if (lastOp.innerText === "") {
+      lastOp.textContent = `${num1} ${btn.textContent}`;
+      currentOp.textContent = "";
+      clear.classList.remove("now-clear");
+    } else if (lastOp.innerText !== "") {
+      num2Arr = [];
+      lastOp.textContent = `${answer} ${btn.textContent}`;
+      currentOp.textContent = "";
+      num1Split = lastOp.textContent.split(" ");
+      num1 = Number(num1Split[0]);
+      clear.classList.remove("now-clear");
+    }
+  });
+});
+
+equal.addEventListener("click", function () {
+  currentOp.textContent = operate(num1Split[1], num1, num2);
+  lastOp.textContent = "";
+  num1 = answer;
+  num2 = 0;
+  num2Arr = [];
+  clear.classList.add("now-clear");
+});
+
+del.addEventListener("click", function () {
+  if (currentOp.textContent !== "" && lastOp.textContent == "") {
+    const currentOpArr = currentOp.innerText.split("");
+    currentOpArr.pop();
+    const currentOpJoin = currentOpArr.join("");
+    currentOp.textContent = currentOpJoin;
+    num1Arr.pop();
+    num1 = Number(num1Arr.join(""));
+  } else if (currentOp.textContent !== "" && lastOp.textContent !== "") {
+    const currentOpArr = currentOp.innerText.split("");
+    currentOpArr.pop();
+    const currentOpJoin = currentOpArr.join("");
+    currentOp.textContent = currentOpJoin;
+    num2Arr.pop();
+    num2 = Number(num2Arr.join(""));
+  }
+});
+
+clear.addEventListener("click", function () {
+  num1Arr = [];
+  num2Arr = [];
+  num1 = 0;
+  num2 = 0;
+  num1Split = [];
+  currentOp.textContent = num1;
+  lastOp.textContent = "";
+  clear.classList.remove("now-clear");
+});
+
+// if (currentOp.textContent === "" && lastOp.textContent !== "") {
+//   // lastOp.remove();
+//   const lastOpArr = lastOp.innerText.split("");
+//   lastOpArr.pop();
+//   const lastOpJoin = lastOpArr.join("");
+//   lastOp.textContent = lastOpJoin;
+// }
